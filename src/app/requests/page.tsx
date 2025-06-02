47c98dfdb79c3570ba8c0319653506f1c7190d5a
jhossepmartinez <jhossepmv@gmail.com>
2025-06-01 20:16:32 -0400

    add logout

--- src/app/requests/page.tsx
+++ src/app/requests/page.tsx
@@ -126,6 +126,7 @@ const SupervisorPage = () => {
 
   return (
     <>
+      <LogoutButton />
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-8">Panel de Supervisor</h1>
         <div className="overflow-x-auto">
